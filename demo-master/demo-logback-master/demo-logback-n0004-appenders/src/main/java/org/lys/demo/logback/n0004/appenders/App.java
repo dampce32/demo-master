3531a65b0a2f8aecea01bab6c5142e3f8b326b5d
dampce32 <linyisong032@163.com>
2016-01-22 14:11:22 +0800

    (+)appenders and layouts

---
+++ demo-master/demo-logback-master/demo-logback-n0004-appenders/src/main/java/org/lys/demo/logback/n0004/appenders/App.java
@@ -0,0 +1,13 @@
+package org.lys.demo.logback.n0004.appenders;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
