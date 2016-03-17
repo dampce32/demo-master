36f2f91ad486b0707ea6e18cae81fcec136b12a0
dampce32 <linyisong032@163.com>
2016-03-17 09:08:17 +0800

    (*)jgroups

--- demo-master/demo-jgroups-master/demo-jgroups-n0001/src/main/java/org/lys/demo/jgroups/SimpleChat.java
+++ demo-master/demo-jgroups-master/demo-jgroups-n0001/src/main/java/org/lys/demo/jgroups/SimpleChat.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
+
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
@@ -11,13 +12,13 @@ import org.jgroups.View;
 import org.jgroups.util.Util;
 
 public class SimpleChat {
-	//
+
 	private JChannel channel;
 	private List<String> state = new LinkedList<String>();
-	private String userName = System.getProperty("user.name", "WhiteSock");
+	private String userName = System.getProperty("user.name");
 
 	public void start() throws Exception {
-		//
+
 		channel = new JChannel();
 		channel.setReceiver(new ReceiverAdapter() {
 			public void receive(Message msg) {
@@ -31,6 +32,7 @@ public class SimpleChat {
 				System.out.println("view accepted: " + view);
 			}
 
+			@SuppressWarnings("unused")
 			public byte[] getState() {
 				synchronized (state) {
 					try {
@@ -42,7 +44,7 @@ public class SimpleChat {
 				}
 			}
 
-			@SuppressWarnings("unchecked")
+			@SuppressWarnings({ "unused", "unchecked" })
 			public void setState(byte[] new_state) {
 				try {
 					List<String> list = (List<String>) Util.objectFromByteBuffer(new_state);
@@ -60,24 +62,42 @@ public class SimpleChat {
 			}
 		});
 		channel.connect("ChatCluster");
-		channel.getState(null, 10000); // sendMessage(); // channel.close(); }
-										// private void sendMessage() throws
-										// Exception { boolean succeed = false;
-										// BufferedReader br = null; try { br =
-										// new BufferedReader(new
-										// InputStreamReader(System.in));
-										// while(true) { System.out.print(">");
-										// System.out.flush(); String line =
-										// br.readLine(); if(line != null &&
-										// line.equals("exit")) { break; } else
-										// { Message msg = new Message(null,
-										// null, "[" + userName + "]" + line);
-										// channel.send(msg); } } succeed =
-										// true; } finally { if(br != null) {
-										// try { br.close(); } catch (Exception
-										// e) { if(succeed) { throw e; } } } } }
-										// public static void main(String
-										// args[]) throws Exception { new
-										// SimpleChat().start(); } }
+		channel.getState(null, 10000);
+		sendMessage();
+		channel.close();
+	}
+
+	private void sendMessage() throws Exception {
+		boolean succeed = false;
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(System.in));
+			while (true) {
+				System.out.print(">");
+				System.out.flush();
+				String line = br.readLine();
+				if (line != null && line.equals("exit")) {
+					break;
+				} else {
+					Message msg = new Message(null, null, "[" + userName + "]" + line);
+					channel.send(msg);
+				}
+			}
+			succeed = true;
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (Exception e) {
+					if (succeed) {
+						throw e;
+					}
+				}
+			}
+		}
+	}
+
+	public static void main(String args[]) throws Exception {
+		new SimpleChat().start();
 	}
 }
