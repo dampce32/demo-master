45985534d85dedeb241737af068fbd1132b23f46
dampce32 <linyisong032@163.com>
2017-04-14 15:28:50 +0800

    (+)requirejs

---
+++ demo-master/demo-jsf-master/demo-jsf-n0001/src/main/java/org/lys/demo/jsf/UserBean.java
@@ -0,0 +1,13 @@
+package org.lys.demo.jsf;
+
+public class UserBean {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+ }  
