1b4a3cdf9cf82a63cb8a078d451141f606efcc79
lin.yisong <lin.yisong@fjlzit.com>
2019-04-09 23:37:42 +0800

    (+)用声明式REST客户端Feign调用远端HTTP服务

---
+++ demo-master/demo-spring-cloud-master/helloworld.feign.client/src/main/java/org/lys/demo/helloworld/feign/client/HelloWorldService.java
@@ -0,0 +1,12 @@
+package org.lys.demo.helloworld.feign.client;
+
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(value = "SERVICE-HELLOWORLD")
+public interface HelloWorldService {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    String sayHello();
+}
