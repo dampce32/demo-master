aca9c9ea0d9ac3257aeeae3ec9c0a8a084116e66
lin.yisong <lin.yisong@fjlzit.com>
2019-04-09 22:59:29 +0800

    （+）Ribbon实现客户端的负载均衡

---
+++ demo-master/demo-spring-cloud-master/helloworld.ribbon.service/src/main/java/org/lys/demo/helloworld/ribbon/service/HelloControler.java
@@ -0,0 +1,14 @@
+package org.lys.demo.helloworld.ribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+    @Autowired HelloService helloService;
+    @RequestMapping(value = "/")
+    public String hello(){
+        return helloService.getHelloContent();
+    }
+}
