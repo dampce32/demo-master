d10b7f307c220907284f784001554a3cba84e3d0
lin.yisong <lin.yisong@fjlzit.com>
2019-05-12 21:12:01 +0800

    (+)rabbitmq

---
+++ demo-master/demo-rabbitmq-master/demo-rabbitmq-n0001/src/main/java/top/linyisong/Recv.java
@@ -0,0 +1,42 @@
+package top.linyisong;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class Recv {
+
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] argv) throws Exception {
+		// 创建连接工厂
+		ConnectionFactory factory = new ConnectionFactory();
+		// 设置RabbitMQ地址
+		factory.setHost("localhost");
+		// 创建一个新的连接
+		Connection connection = factory.newConnection();
+		// 创建一个通道
+		Channel channel = connection.createChannel();
+		// 声明要关注的队列
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+		// DefaultConsumer类实现了Consumer接口，通过传入一个频道，
+		// 告诉服务器我们需要那个频道的消息，如果频道中有消息，就会执行回调函数handleDelivery
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+					byte[] body) throws IOException {
+				String message = new String(body, "UTF-8");
+				System.out.println("Customer Received '" + message + "'");
+			}
+		};
+		// 自动回复队列应答 -- RabbitMQ中的消息确认机制
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+	}
+}
